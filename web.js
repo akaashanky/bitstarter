c1c52d3f5f9f3151ab616e4fc899534ff3aeb814
Akaash Patnaik <akaashanky@gmail.com>
2013-07-10 22:33:23 +0000

    Modified web.js to read from buffer

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
  var msg = fs.readFileSync('index.html'); 
- response.send(msg);
+ response.send(msg.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
